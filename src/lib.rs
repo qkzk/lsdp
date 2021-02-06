18e90e3c1f6e5f5c1a14b8059c5af9c3ef7fe81c
qu3nt1n@gmail.com <qu3nt1n@gmail.com>
2021-02-06 22:32:05 +0100

    relecture :)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::error::Error;
 use std::fs;
-use terminal_size::{Width, terminal_size};
+use terminal_size::{terminal_size, Width};
 
 //use std::path::Path;
 
@@ -11,8 +11,7 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String])
-        -> Result<Config, &'static str> {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         //println!("Config - args: {:?}", &args);
         if args.len() == 1 {
             Ok(Config {
@@ -28,10 +27,7 @@ impl Config {
         } else {
             let path = args[2].clone();
             //let hidd_str = args[1].clone();
-            Ok(Config {
-                hidden: true,
-                path,
-            })
+            Ok(Config { hidden: true, path })
         }
     }
 }
@@ -60,34 +56,32 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn maxsize(content: &Vec<String>) -> usize {
-    let content_count: Vec<usize> = content.iter().map(|x| x.chars()
-                                                            .count()
-                                                      ).collect();
-    content_count.iter()
-                 .fold(content_count[0], |acc, &item| {
-        if item > acc {
-            item
-        } else {
-            acc
-        }
-    })
+    let content_count: Vec<usize> = content.iter().map(|x| x.chars().count()).collect();
+    content_count.iter().fold(
+        content_count[0],
+        |acc, &item| {
+            if item > acc {
+                item
+            } else {
+                acc
+            }
+        },
+    )
 }
 
 pub fn pad_filename(filename: &String, width: usize) -> String {
-    String::from(format!("{:<width$}", filename, width=width))
+    String::from(format!("{:<width$}", filename, width = width))
 }
 
 pub fn term_size() -> u16 {
-    if let Some((Width(w), _)) = terminal_size(){
+    if let Some((Width(w), _)) = terminal_size() {
         w
     } else {
         80
     }
 }
 
-
 pub fn pad_strings(content: Vec<String>, max_string_size: usize) -> Vec<String> {
-
     content
         .iter()
         .map(|s| pad_filename(&s, max_string_size))
@@ -95,7 +89,6 @@ pub fn pad_strings(content: Vec<String>, max_string_size: usize) -> Vec<String> 
 }
 
 pub fn format_bloc(content_formatted: Vec<String>, max_string_size: usize) -> String {
-
     let max_string_size = max_string_size as u16;
     let max_item_per_line = term_size() / max_string_size;
     let mut string_output = String::new();
