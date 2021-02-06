77e26ced16e68fae8134f944bdb3fe22efb57d1b
qu3nt1n@gmail.com <qu3nt1n@gmail.com>
2021-02-06 14:29:56 +0100

    simplest i can write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut content: Vec<String> = vec![];
+    for entry in fs::read_dir(".").unwrap() {
+        match entry {
+            Ok(direntry) => {
+                let thing = direntry.file_name().into_string().unwrap();
+                content.push(thing);
+            },
+            Err(e) => println!("Something went wrong {}", e),
+        }
+    }
+
+    let mut stdoutput = String::new();
+    for name in &content {
+        stdoutput.push_str(&name);
+        stdoutput.push_str(" ");
+    }
+    println!("{}", stdoutput);
 }
