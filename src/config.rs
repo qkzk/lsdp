6d48ef3a06615004f52a5a4b637704bfc7177f12
qu3nt1n@gmail.com <qu3nt1n@gmail.com>
2021-02-07 20:52:53 +0100

    refactor config

--- src/config.rs
+++ src/config.rs
@@ -18,32 +18,13 @@ impl Default for Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        let vec_options: Vec<String> = vec![String::from("a"), String::from("l")];
         //println!("Config - args: {:?}", &args);
         if args.len() == 1 {
             Ok(Config {
                 ..Default::default()
             })
         } else {
-            let mut map_options = HashMap::new();
-            let mut path = String::from(".");
-            for opt in vec_options.iter() {
-                map_options.insert(opt, false);
-            }
-
-            for arg in args[1..].iter() {
-                if arg.starts_with("-") {
-                    let option = String::from(&arg[1..]);
-                    let arg_characs: Vec<&str> = option.split("").collect();
-                    for option in vec_options.iter() {
-                        if arg_characs.contains(&&option[..]) {
-                            map_options.insert(option, true);
-                        }
-                    }
-                } else {
-                    path = String::from(arg);
-                }
-            }
+            let (path, map_options) = parse_args(&args);
             Ok(Config {
                 hidden: map_options[&String::from("a")],
                 list: map_options[&String::from("l")],
@@ -52,4 +33,28 @@ impl Config {
             })
         }
     }
+}
+
+pub fn parse_args(args: &[String]) -> (String, HashMap<String, bool>) {
+    let vec_options: Vec<String> = vec![String::from("a"), String::from("l")];
+    let mut map_options = HashMap::new();
+    let mut path = String::from(".");
+    for opt in vec_options.iter() {
+        map_options.insert(String::from(opt), false);
+    }
+
+    for arg in args[1..].iter() {
+        if arg.starts_with("-") {
+            let option = String::from(&arg[1..]);
+            let arg_characs: Vec<&str> = option.split("").collect();
+            for option in vec_options.iter() {
+                if arg_characs.contains(&&option[..]) {
+                    map_options.insert(String::from(option), true);
+                }
+            }
+        } else {
+            path = String::from(arg);
+        }
+    }
+    (path, map_options)
 }
