53273d237c8033758fdf7bf6ef2516c80efe6522
qu3nt1n@gmail.com <qu3nt1n@gmail.com>
2021-02-07 19:01:36 +0100

    fileinfo

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
-use fs::{metadata, DirEntry};
 use std::error::Error;
-use std::fs;
-use std::os::unix::fs::MetadataExt;
-use std::os::unix::fs::PermissionsExt;
+use std::fs::read_dir;
 use std::{collections::HashMap, println};
 use terminal_size::{terminal_size, Width};
-use users::get_user_by_uid;
 use std::process;
 
+mod extract;
+
+//pub use crate::extract;
 
 #[derive(Debug)]
 pub struct Config {
@@ -55,13 +54,13 @@ impl Config {
             for arg in args[1..].iter() {
                 if arg.starts_with("-") {
                     let option = String::from(&arg[1..]);
-                    println!("{}", option);
+                    //println!("{}", option);
                     let arg_characs: Vec<&str> = option.split("").collect();
-                    println!("characs : {:?}", arg_characs);
+                    //println!("characs : {:?}", arg_characs);
                     for option in vec_options.iter() {
-                        println!("option : {}", option);
+                        //println!("option : {}", option);
                         if arg_characs.contains(&&option[..]) {
-                            println!("found option {}", option);
+                            //println!("found option {}", option);
                             map_options.insert(option, true);
                         }
                     }
@@ -69,7 +68,7 @@ impl Config {
                     path = String::from(arg);
                 }
             }
-            println!("map options {:?}", map_options);
+            //println!("map options {:?}", map_options);
             Ok(Config {
                 hidden: map_options[&String::from("a")],
                 list: map_options[&String::from("l")],
@@ -90,43 +89,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     }
 }
 
-pub struct FileInfo {
-    pub filename: String,
-    pub file_size: String,
-    pub dir_symbol: String,
-    pub permissions: String,
-    pub owner: String,
-}
-
-impl FileInfo {
-    pub fn new(direntry: &DirEntry) -> Result<FileInfo, &'static str> {
-        let filename = extract_filename(&direntry);
-        let file_size = human_size(extract_file_size(&direntry));
-        let dir_symbol = extract_dir_symbol(&direntry);
-        let permissions = extract_permissions_string(&direntry);
-        let owner = extract_username(&direntry);
-
-        Ok(FileInfo {
-            filename,
-            file_size,
-            dir_symbol,
-            permissions,
-            owner,
-        } )
-    }
-}
 
 pub fn display_list(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("{:?}", config);
+    //println!("{:?}", config);
     let path = &config.path;
     //let mut content: Vec<(String, String, String, String, String)> = vec![];
-    let mut content_file_info: Vec<FileInfo> = vec![];
-    for entry in fs::read_dir(&path)? {
+    let mut content_file_info: Vec<extract::FileInfo> = vec![];
+    for entry in read_dir(&path)? {
         match entry {
             Ok(direntry) => {
-                let filename = extract_filename(&direntry);
+                let filename = extract::extract_filename(&direntry);
                 if config.hidden || !filename.starts_with(".") {
-                    let file_info = FileInfo::new(&direntry).unwrap_or_else(|err| {
+                    let file_info = extract::FileInfo::new(&direntry).unwrap_or_else(|err| {
                         eprintln!("Problem accessing file {} information: {}",
                             filename, err);
                         process::exit(1);
@@ -141,60 +115,8 @@ pub fn display_list(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn extract_filename(direntry: &DirEntry) -> String {
-    direntry.file_name().into_string().unwrap()
-}
 
-pub fn extract_permissions_string(direntry: &DirEntry) -> String {
-    let mode = metadata(direntry.path()).unwrap().permissions().mode() & 511;
-
-    let mode_o = mode >> 6;
-    let mode_g = (mode >> 3) & 7;
-    let mode_a = mode & 7;
-
-    let s_o = convert_octal_mode(mode_o);
-    let s_g = convert_octal_mode(mode_g);
-    let s_a = convert_octal_mode(mode_a);
-
-    [s_o, s_g, s_a].join("")
-}
-
-pub fn convert_octal_mode(mode: u32) -> String {
-    let rwx  = ["---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"];
-    String::from(rwx[(mode & 7 as u32) as usize])
-}
-
-pub fn extract_username(direntry: &DirEntry) -> String {
-    let meta = metadata(direntry.path());
-    let owner_id: u32 = meta.unwrap().uid();
-    let user = get_user_by_uid(owner_id).unwrap();
-    String::from(user.name().to_str().unwrap())
-}
-
-pub fn extract_dir_symbol(direntry: &DirEntry) -> String {
-    let meta = metadata(direntry.path());
-    if meta.unwrap().is_dir() {
-        String::from("d")
-    } else {
-        String::from(".")
-    }
-}
-
-pub fn extract_file_size(direntry: &DirEntry) -> u64 {
-    direntry.path().metadata().unwrap().len()
-}
-
-pub fn human_size(bytes: u64) -> String {
-    let size = ["", "k", "M", "G", "T", "P", "E", "Z", "Y"];
-    let factor = (bytes.to_string().chars().count() as u64 - 1) / 3  as u64;
-    let human_size = format!("{:>3}{:<1}",
-                bytes / (1204 as u64).pow(factor as u32),
-                size[factor as usize]
-            );
-    human_size
-}
-
-pub fn stdout_list(content_file_info: Vec<FileInfo>) {
+pub fn stdout_list(content_file_info: Vec<extract::FileInfo>) {
     //let first_line = "Permissions Size User    Date Modified  Name";
     //println!("{}", first_line);
     for file_info in content_file_info.iter() {
@@ -212,10 +134,10 @@ pub fn stdout_list(content_file_info: Vec<FileInfo>) {
 pub fn display_column(config: Config) -> Result<(), Box<dyn Error>> {
     let path = &config.path;
     let mut content: Vec<String> = vec![];
-    for entry in fs::read_dir(&path)? {
+    for entry in read_dir(&path)? {
         match entry {
             Ok(direntry) => {
-                let filename = extract_filename(&direntry);
+                let filename = extract::extract_filename(&direntry);
                 if config.hidden || !filename.starts_with(".") {
                     content.push(filename);
                 }
